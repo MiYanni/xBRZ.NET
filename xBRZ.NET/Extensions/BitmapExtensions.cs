22a9bfe33b0299a19ff2a0ca68da4875cbcb9d23
Michael Yanni <miyanni@microsoft.com>
2016-08-16 13:31:19 -0700

    More cleanup. Put logical Bitmap extensions into their own methods.

--- xBRZ.NET/Extensions/BitmapExtensions.cs
+++ xBRZ.NET/Extensions/BitmapExtensions.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace xBRZ.NET.Extensions
 {
@@ -19,6 +15,48 @@ namespace xBRZ.NET.Extensions
                 gr.DrawImage(image, new Rectangle(0, 0, newFormatImage.Width, newFormatImage.Height));
             }
             return newFormatImage;
+        }
+
+        //https://msdn.microsoft.com/en-us/library/ms229672(v=vs.90).aspx
+        public static int[] ToIntArray(this Bitmap image)
+        {
+            // Lock the bitmap's bits.
+            var rectangle = new Rectangle(0, 0, image.Width, image.Height);
+            var bitmapData = image.LockBits(rectangle, ImageLockMode.ReadWrite, image.PixelFormat);
+            // Get the address of the first line.
+            var bitmapPointer = bitmapData.Scan0;
+            //http://stackoverflow.com/a/13273799/294804
+            if (bitmapData.Stride < 0)
+            {
+                bitmapPointer += bitmapData.Stride * (image.Height - 1);
+            }
+            //http://stackoverflow.com/a/1917036/294804
+            // Declare an array to hold the bytes of the bitmap. 
+            var intCount = bitmapData.Stride * image.Height / 4;
+            var values = new int[intCount];
+            // Copy the RGB values into the array.
+            Marshal.Copy(bitmapPointer, values, 0, intCount);
+            // Unlock the bits.
+            image.UnlockBits(bitmapData);
+
+            return values;
+        }
+
+        public static Bitmap ToBitmap(this int[] bitmapData, int width, int height, PixelFormat format)
+        {
+            var newImage = new Bitmap(width, height, format);
+            var rectangle = new Rectangle(0, 0, newImage.Width, newImage.Height);
+            var newBitmapData = newImage.LockBits(rectangle, ImageLockMode.ReadWrite, format);
+            // Get the address of the first line.
+            var newBitmapPointer = newBitmapData.Scan0;
+            //http://stackoverflow.com/a/1917036/294804
+            var intCount = newBitmapData.Stride * newImage.Height / 4;
+            // Copy the RGB values back to the bitmap
+            Marshal.Copy(bitmapData, 0, newBitmapPointer, intCount);
+            // Unlock the bits.
+            newImage.UnlockBits(newBitmapData);
+
+            return newImage;
         }
     }
 }
--- xBRZ.NET/xBRZScaler.cs
+++ xBRZ.NET/xBRZScaler.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
+using xBRZ.NET.Extensions;
 
 namespace xBRZ.NET
 {
@@ -52,68 +53,17 @@ namespace xBRZ.NET
         public Bitmap ScaleImage(Bitmap image, ScaleSize size, ScalerCfg config = null)
         {
             config = config ?? new ScalerCfg();
-            // Specify a pixel format.
-            const PixelFormat pxf = PixelFormat.Format32bppRgb;
 
-            //http://stackoverflow.com/a/2016509/294804
-            var fixedFormatImage = new Bitmap(image.Width, image.Height, pxf);
-            using (var gr = Graphics.FromImage(fixedFormatImage))
-            {
-                gr.DrawImage(image, new Rectangle(0, 0, fixedFormatImage.Width, fixedFormatImage.Height));
-            }
-
-            //https://msdn.microsoft.com/en-us/library/ms229672(v=vs.90).aspx
-            // Lock the bitmap's bits.
-            var rect = new Rectangle(0, 0, fixedFormatImage.Width, fixedFormatImage.Height);
-            var bmpData = fixedFormatImage.LockBits(rect, ImageLockMode.ReadWrite, pxf);
-
-            // Get the address of the first line.
-            var ptr = bmpData.Scan0;
-
-            if (bmpData.Stride < 0)
-            {
-                Console.WriteLine("Upside-down");
-                //http://stackoverflow.com/a/13273799/294804
-                ptr += bmpData.Stride * (fixedFormatImage.Height - 1);
-            }
-
-            //http://stackoverflow.com/a/1917036/294804
-            // Declare an array to hold the bytes of the bitmap. 
-            var numBytes = bmpData.Stride * fixedFormatImage.Height;
-            var rgbValues = new int[numBytes / 4];
-
-            // Copy the RGB values into the array.
-            Marshal.Copy(ptr, rgbValues, 0, numBytes / 4);
-
-            // Unlock the bits.
-            fixedFormatImage.UnlockBits(bmpData);
+            var fixedFormatImage = image.ChangeFormat(PixelFormat.Format32bppRgb);
+            var rgbValues = fixedFormatImage.ToIntArray();
 
             var scaleFactor = size.Size;
-            var scaledRbgValues = new int[numBytes / 4 * (scaleFactor * scaleFactor)];
+            var scaledRbgValues = new int[rgbValues.Length * (scaleFactor * scaleFactor)];
 
             ScaleImage(size, rgbValues, scaledRbgValues, fixedFormatImage.Width, fixedFormatImage.Height, config, 0, int.MaxValue);
 
-            var scaledImage = new Bitmap(fixedFormatImage.Width * scaleFactor, fixedFormatImage.Height * scaleFactor, pxf);
-            var rect2 = new Rectangle(0, 0, scaledImage.Width, scaledImage.Height);
-            var bmpData2 = scaledImage.LockBits(rect2, ImageLockMode.ReadWrite, pxf);
-
-            // Get the address of the first line.
-            var ptr2 = bmpData2.Scan0;
-
-            //http://stackoverflow.com/a/1917036/294804
-            // Declare an array to hold the bytes of the bitmap. 
-            var numBytes2 = bmpData2.Stride * scaledImage.Height;
-
-            // Copy the RGB values back to the bitmap
-            Marshal.Copy(scaledRbgValues, 0, ptr2, numBytes2 / 4);
-
-            // Unlock the bits.
-            scaledImage.UnlockBits(bmpData);
-
-            return scaledImage;
+            return scaledRbgValues.ToBitmap(fixedFormatImage.Width * scaleFactor, fixedFormatImage.Height * scaleFactor, fixedFormatImage.PixelFormat);
         }
-
-
 
         public void ScaleImage(ScaleSize scaleSize, int[] src, int[] trg, int srcWidth, int srcHeight, ScalerCfg cfg, int yFirst, int yLast)
         {
