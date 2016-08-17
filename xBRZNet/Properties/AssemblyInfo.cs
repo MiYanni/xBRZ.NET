c008cb9490a1b504c5478af7c7df444757e8722c
Michael Yanni <miyanni@microsoft.com>
2016-08-17 14:51:28 -0700

    Minor assembly naming fix.

--- xBRZNet/Properties/AssemblyInfo.cs
+++ xBRZNet/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("xBRZ.NET")]
+[assembly: AssemblyTitle("xBRZNet")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("xBRZ.NET")]
+[assembly: AssemblyProduct("xBRZNet")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
